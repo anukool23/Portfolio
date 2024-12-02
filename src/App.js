544d6c319e4feb09b99a7f219796e89469772c29
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2024-12-02 23:56:25 +0530

    Added Projects and About in App.js to make it on single page

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ function App() {
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
+        <About />
+        <Projects />
         <Footer />
       </div>
     </Router>
