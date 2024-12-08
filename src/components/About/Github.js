08ac7226325b439da04d5505d0d5a5c8dc6e7b31
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2024-12-09 05:16:52 +0530

    Updates all versions and dependencies

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,21 +1,35 @@
 import React from "react";
-import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
+import { GitHubCalendar } from "github-contribution-calendar";
+
+
 
 function Github() {
+    const my_theme = {
+      noContributions: "rgba(190, 130, 230, 0.2)",
+      low: "#8E5BB7",  // Deep vibrant purple
+      moderate: "#9D5FDB",  // Vivid medium purple
+      high: "#E0569E",  // Bright magenta
+      veryHigh: "#F3794A",
+      showTotalContributions:true  // Bright coral orange
+        
+    }
+
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+    <div className="App">
+         <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
-        username="anukool23"
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
-      />
-    </Row>
+        <GitHubCalendar 
+        username={process.env.REACT_APP_GITHUB_USERNAME}
+        token={process.env.REACT_APP_GITHUB_TOKEN}
+         theme="forest"
+        // theme="custom" // must be specified for custom theme to be applied 
+        // customTheme={my_theme}
+        showTotalContributions={true}
+        background={"##110d1a"}
+        labelColor = {"white"}
+        titleColor = {"white"} />
+    </div>
   );
 }
 
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  // SiVscodium,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
-} from "react-icons/si";
+import { SiPostman, SiSlack, SiVercel,SiMacos,} from "react-icons/si";
+import { DiVisualstudio} from "react-icons/di";
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiMacos />
+        <p className="tech-text">Mac OS</p>
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiVscodium />
-      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiVisualstudio />
+        <p className="tech-text">VS Code</p>
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <p className="tech-text">Postman</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
+        <p className="tech-text">Slack</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <p className="tech-text">Vercel</p>
       </Col>
     </Row>
   );
